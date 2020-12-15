78314b8cbb57fd15f7ffdc1d03a3fb4735d8213d
Carles Homs <36209557+ch0m5@users.noreply.github.com>
2020-12-15 18:34:54 +0100

    Created Empty Events

---
+++ DataAnalysisPractice2/Assets/3DGamekitLite/Scripts/Scripts/EventHandler.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // PLAYER EVENTS
+    // Recurrent events
+    public void AddNewWalkingPositionEvent()
+    {
+
+    }
+
+    public void AddNewAirbornePositionEvent()
+    {
+
+    }
+
+    public void AddNewKeyPositionEvent()
+    {
+
+    }
+
+    // Trigger events
+    public void AddNewJumpEvent()
+    {
+
+    }
+
+    public void AddNewAttackEvent()
+    {
+
+    }
+
+    public void AddNewDamageEvent()
+    {
+
+    }
+
+    public void AddNewDeathEvent()
+    {
+
+    }
+
+    public void AddNewSpawnEvent()
+    {
+
+    }
+
+    public void AddNewInvulnerabilityStartEvent()
+    {
+
+    }
+
+    public void AddNewInvulnerabilityEndEvent()
+    {
+
+    }
+
+    // ENEMY EVENTS
+    // Recurrent events
+    public void AddNewPositionEvent()
+    {
+
+    }
+
+    // Trigger events
+    public void AddAttackEvent()
+    {
+
+    }
+
+    public void AddDeathEvent()
+    {
+
+    }
+}
